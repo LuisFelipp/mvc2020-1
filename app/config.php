6daea6ebf9d1caad149e33b01d9e42d27ccd5e71
LuisFelipp <67172546+LuisFelipp@users.noreply.github.com>
2020-07-03 20:10:41 -0300

    Commit

--- app/config.php
+++ app/config.php
@@ -1,20 +1,36 @@
 <?php
+
 /**
- * @constante DATABASE Valores para conexao ao banco de dados em produção
- */
+*@constante DATABASE valores para conexao ao banco em producao
+*/
+
+//Configuracao Banco de Dados Local
+//define("HOST",     "localhost");
+//define("USER",     "root");
+//define("PASSWD",   "");
+//define("DATABASE", "nome_banco");
+
+//Configuracoes Banco no 000webhost
+//define("HOST",     "localhost");
+//define("USER",     "id");
+//define("PASSWD",   "");
+//define("DATABASE", "id");
+
 define('DATABASE', [
-    'host' =>     'den1.mysql4.gear.host',
-    'dbname' =>   'db3venda1',
-    'user' =>     'db3venda1',
-    'password' => 'Vn214-Ctvf?w',
+    'host' => 'localhost',
+    'dbname' => 'id13594392_aulab3',
+    'user' => 'id13594392_thiago',
+    'password' => '/a01^e|T{ivBf6Hs',
     'options' => [
         PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_CASE => PDO::CASE_NATURAL,
-        PDO::ATTR_EMULATE_PREPARES => false
+        PDO::ATTR_EMULATE_PREPARES, false
     ],
 ]);
 
+//timezone
 date_default_timezone_set('America/Sao_Paulo');
 
-define('ROOT','http://localhost/mvc2020/app/views/assets/');
+//urls
+define('ROOT' , 'http://localhost/MVC2020/app/views/assets/');
